8f7ceae84c5fe3000c25f638f6698123f4c9e54c
herlanw <herlan@ruangguru.com>
2022-09-16 11:38:47 +0700

    test

---
+++ backend/models/UserModel.js
@@ -0,0 +1,17 @@
+import { DatabaseError, DataTypes, Sequelize } from "sequelize";
+import db from "../config/Database.js";
+
+const User = db.define('users', {
+    name: DataTypes.STRING,
+    email:DataTypes.STRING,
+    gender:DataTypes.STRING
+},{
+    freezeTableName:true
+});
+
+export default User;
+
+(async()=> {
+     await db.sync();
+})();
+
